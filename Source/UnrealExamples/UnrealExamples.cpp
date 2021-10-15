27f961b24bac2fbd6bb826dec28149b736dac783
Sam Swain <sam@apparance.uk>
2021-10-15 09:55:10 +0100

    Clean-up for 4.25, and running plugin-less

--- Source/UnrealExamples/UnrealExamples.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Apparance Studios 2021
-
-#include "UnrealExamples.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UnrealExamples, "UnrealExamples" );
--- Source/UnrealExamples/UnrealExamples.h
+++
@@ -1,6 +0,0 @@
-// Copyright Apparance Studios 2021
-
-#pragma once
-
-#include "CoreMinimal.h"
-
